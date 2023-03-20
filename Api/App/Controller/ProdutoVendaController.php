f2ccca217c8118eca35096ccac782b3bc6fe1d57
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-03-20 14:24:34 -0300

    feat: crud produtoVenda

---
+++ Api/App/Controller/ProdutoVendaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ProdutoVendaModel;
+use App\Controller\Controller;
+
+class ProdutoVendaController extends Controller
+{
+    public static function index()
+    {
+        $model = new ProdutoVendaModel();
+        $model->getAllRows();
+
+        parent::setResponseAsJSON($model);
+    }
+
+    public static function form()
+    {
+        $model = new ProdutoVendaModel();
+
+        if(isset($_GET['id']))
+            $model = $model->getById( (int) $_GET['id']);
+        
+    }
+
+    public static function save()
+    {
+        $produto_venda = new ProdutoVendaModel();
+
+        $produto_venda->id = $_POST['id'];                          
+        $produto_venda->quantidade = $_POST['quantidade'];     
+        $produto_venda->id_produto = $_POST['id_produto'];     
+        $produto_venda->id_venda = $_POST['id_venda'];          
+
+        $produto_venda->save();
+
+        parent::setResponseAsJSON($produto_venda);
+    }
+
+    public static function delete()
+    {
+        $model = new ProdutoVendaModel();
+
+        $model->delete( (int) $_GET['id']);
+
+        parent::setResponseAsJSON($model);
+    }
+}
---
+++ Api/App/DAO/ProdutoVendaDAO.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DAO;
+
+use App\Model\ProdutoVendaModel;
+use \PDO;
+
+class ProdutoVendaDAO extends DAO
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function insert(ProdutoVendaModel $model)
+    {
+        $sql = "INSERT INTO produto_venda (quantidade, id_produto, id_venda) VALUE (?, ?, ?)";
+
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->bindValue(1, $model->quantidade);        
+        $stmt->bindValue(2, $model->id_produto);        
+        $stmt->bindValue(3, $model->id_venda);         
+
+        $stmt->execute();
+    }
+
+    public function update(ProdutoVendaModel $model)
+    {
+        $sql = "UPDATE produto_venda SET quantidade = ?, id_produto = ?, id_venda = ? WHERE id = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+   
+        $stmt->bindValue(1, $model->quantidade);
+        $stmt->bindValue(2, $model->id_produto);
+        $stmt->bindValue(3, $model->id_venda);
+        $stmt->bindValue(4, $model->id);
+
+        $stmt->execute();
+    }
+
+    public function select()
+    {
+        $sql = "SELECT * FROM produto_venda";
+
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS);
+    }
+
+    public function selectById(int $id)
+    {
+        $sql = "SELECT * FROM produto_venda WHERE id = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+
+        $stmt->execute();
+
+        return $stmt->fetchObject("App\Model\ProdutoVendaModel");
+    }
+
+    public function delete(int $id)
+    {
+        $sql = "DELETE FROM produto_venda WHERE id = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+        
+        $stmt->execute();
+    }
+}
