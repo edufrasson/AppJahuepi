81471e037478f6503557f11e33ce7f15c67b4523
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-03-20 14:09:51 -0300

    feat: adicionando funções

--- Api/App/Controller/Controller.php
+++ Api/App/Controller/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Exception;
+
 abstract class Controller{
     /* Retorna um valor como um objeto JSON*/
     protected static function setResponseAsJSON($data, $request_status = true)
@@ -30,5 +32,69 @@ abstract class Controller{
         header("Pragma: public");      
         
         return json_decode(file_get_contents('php://input'));
+    }
+
+    public static function getResponseAsJSON($data)
+    {
+        header("Acess-Control-Allow-Origin: *");
+        header("Content-type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GHT");
+
+        exit(json_encode($data));
+    }
+
+    protected static function getExceptionAsJSON(Exception $e)
+    {
+        $exception = [
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'traceAsString' => $e->getTraceAsString(),
+            'previous' => $e->getPrevious()
+        ];
+
+        http_response_code(400);
+
+        header("Acess-Control-Allow-Origin: *");
+        header("Content-type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");
+
+        exit(json_encode($exception));
+    }
+
+    protected static function isGet()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'GET')
+            throw new Exception("O método da requisição deve ser GET");
+    }
+
+    protected static function  isPost()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST')
+            throw new Exception("O método da requisição deve de POST");
+    }
+
+    protected static function getIntFromUrl($var_get, $var_name = null) : int
+    {
+        self::isGet();
+
+        if(!empty($var_get))
+            return (int) $var_get;
+        else
+            throw new Exception("Variável $var_name não identificada.");
+    }
+
+    protected static function getStringFromUrl($var_get, $var_name = null) : string
+    {
+        self::isGet();
+
+        if(!empty($var_get))
+            return (string) $var_get;
+        else
+            throw new Exception("Variável $var_name não identificada.");
     }
 }
