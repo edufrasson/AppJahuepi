b4090e91b1abc8d951b71aad7cf43d58e049533e
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-03-27 14:58:44 -0300

    feat: rotas

--- Api/App/routes.php
+++ Api/App/routes.php
@@ -10,10 +10,12 @@ use App\Controller\{
     LoginController,
     CategoriaProdutoController
 };
+use App\DAO\CategoriaProdutoDAO;
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($url) {
+
     case '/produto':
         ProdutoController::index();
         break;
@@ -26,6 +28,83 @@ switch ($url) {
         ParcelaController::index();
         break;
 
+    case '/pagamento':
+        PagamentoController::index();
+        break;
+
+    case '/categoria/produto':
+        CategoriaProdutoController::index();
+        break;
+
+    case '/venda':
+        VendaController::index();
+        break;
+
+    case '/produto/venda':
+        ProdutoVendaController::index();
+        break;
+
+
+    case '/produto/save':
+        ProdutoController::save();
+        break;
+
+    case '/extrato/save':
+        ExtratoController::save();
+        break;
+
+    case '/parcela/save':
+        ParcelaController::save();
+        break;
+
+    case '/pagamento/save':
+        PagamentoController::save();
+        break;
+
+    case '/categoria/produto/save':
+        CategoriaProdutoController::save();
+        break;
+
+    case '/venda/save':
+        VendaController::save();
+        break;
+
+    case '/produto/venda/save':
+        ProdutoVendaController::save();
+        break;
+
+
+    case '/produto/delete':
+        ProdutoController::delete();
+        break;
+
+    case '/extrato/delete':
+        ExtratoController::delete();
+        break;
+
+    case '/parcela/delete':
+        ParcelaController::delete();
+        break;
+
+    case '/pagamento/delete':
+        PagamentoController::delete();
+        break;
+
+    case '/categoria/produto/save/delete':
+        CategoriaProdutoController::delete();
+        break;
+
+    case '/venda/delete':
+        VendaController::delete();
+        break;
+
+    case '/produto/venda/delete':
+        ProdutoVendaController::delete();
+        break;
+
+
+    
+    
     case '/auth':
         LoginController::auth();
         break;   
