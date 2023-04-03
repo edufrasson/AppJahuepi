a1fa395e3141d03a3dd4b124a547319c5c845d69
edufrasson <edudomine2016@gmail.com>
2023-04-03 14:04:52 -0300

    feat: estruturacao do front

--- Api/App/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-define('BASEDIR', dirname(__FILE__, 2));
-define('VIEWS', BASEDIR . '/App/View/modules/');
-
-$_ENV['db']['host'] = 'localhost:3307';
-$_ENV['db']['user'] = 'root';
-$_ENV['db']['pass'] = 'etecjau';
-$_ENV['db']['database'] = 'db_jahuepi';
