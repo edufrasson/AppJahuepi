d8201373b08ff1252c353ac1d10f0023efc4a9cd
edufrasson <edudomine2016@gmail.com>
2023-04-10 13:58:17 -0300

    feat: plugin DataTable

--- Site/App/View/includes/css_config.php
+++ Site/App/View/includes/css_config.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="View/bootstrap/dist/css/bootstrap.min.css">
+<script src="View/js/DataTable/datables.min.css"></script>
 <link rel="stylesheet" href="View/styles/global.css">
---
+++ Site/App/View/js/src/jquery.global.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.table-style').DataTable({
+        "language": {
+            "info": "Mostrando _START_ - _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrando 0 - 0 de 0 registros",
+            "infoFiltered":   "(Filtrado em _MAX_ registros totais)",
+            "paginate": {
+                "first":      "Primeiro",
+                "last":       "Último",
+                "next":       "Próximo",
+                "previous":   "Anterior"
+            },
+            "search": "Pesquisar:",
+            "lengthMenu":     "Mostrar _MENU_ registros",
+            "zeroRecords": "Nenhum registro encontrado!",
+          }
+    });
+
+    
+})
