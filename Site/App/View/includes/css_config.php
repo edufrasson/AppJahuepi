a0f16009582f50b15ef45a6a0f128c0a12d73dcf
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-04-17 15:13:34 -0300

    feat: favicon

--- Site/App/View/includes/css_config.php
+++ Site/App/View/includes/css_config.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="View/bootstrap/dist/css/bootstrap.min.css">
 <script src="View/js/DataTable/datables.min.css"></script>
 <link rel="stylesheet" href="View/styles/global.css">
+<link rel="shortcut icon" href="View/assets/icone.png" type="image/x-icon">
 
 <!-- JQuery DataTable -->
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.13.2/b-2.3.4/cr-1.6.1/r-2.4.0/rr-1.3.2/sc-2.1.0/sb-1.4.0/datatables.min.css"/>
