c6a5d8ca6c71072b7d9360ea22c352628ce7a3be
Eduardo Frasson <edudomine2016@gmail.com>
2023-04-20 13:30:41 -0300

    feat: reorganizando rotas

--- Site/App/View/js/src/jquery.categoria.js
+++ Site/App/View/js/src/jquery.categoria.js
@@ -1,7 +1,5 @@
-function addCategoria(id, descricao)
-{
-    if(descricao !== "")
-    {
+function addCategoria(id, descricao) {
+    if (descricao !== "") {
         $.ajax({
             type: "POST",
             url: "/categoria_produto/save",
@@ -20,8 +18,7 @@ function addCategoria(id, descricao)
     }
 }
 
-function getCategoriaById(id)
-{
+function getCategoriaById(id) {
     $.ajax({
         type: "GET",
         url: "/categoria_produto/get-by-id?id=" + id,
@@ -36,8 +33,7 @@ function getCategoriaById(id)
     });
 }
 
-function deleteCategoria(id)
-{
+function deleteCategoria(id) {
     $.ajax({
         type: "GET",
         url: "/categoria_produto/delete?id=" + id,
@@ -51,12 +47,32 @@ function deleteCategoria(id)
     });
 }
 
-$(document).ready(function (){
-    $('.btn-edit').click(function(event){
+$(document).ready(function () {
+    $('.table-style').DataTable({
+        "language": {
+            "info": "Mostrando _START_ - _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrando 0 - 0 de 0 registros",
+            "infoFiltered": "(Filtrado em _MAX_ registros totais)",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "search": "Pesquisar:",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "Nenhum registro encontrado!",
+        },
+        "ajax": {
+            "url": "/categoria/get-all"
+        }
+    });
+
+    $('.btn-edit').click(function (event) {
         getCategoriaById(event.target.id);
     })
 
-    $('.btn-delete').click(function(event){
+    $('.btn-delete').click(function (event) {
         deleteCategoria(event.target.id);
 
         window.location.reload(true);
--- Site/App/routes.php
+++ Site/App/routes.php
@@ -16,142 +16,121 @@ $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($url) {
 
-    /* INDEX */
+    /* Produto */
 
     case '/produto':
         ProdutoController::index();
         break;
+    case '/produto/save':
+        ProdutoController::save();
+        break;
+    case '/produto/get-by-id':
+        ProdutoController::getById();
+        break;
+    case '/produto/delete':
+        ProdutoController::delete();
+        break;
 
+    /* Login*/
     case '/login':
         LoginController::form();
-        break;    
+        break;
+    case '/login/auth':
+        LoginController::auth();
+        break;
 
+    /* Extrato*/
     case '/extrato':
         ExtratoController::index();
         break;
+    case '/extrato/save':
+        ExtratoController::save();
+        break;
+    case '/extrato/get-by-id':
+        ExtratoController::getById();
+        break;
+    case '/extrato/delete':
+        ExtratoController::delete();
+        break;
+
+    /* Parcela */
 
     case '/parcela':
         ParcelaController::index();
         break;
+    case '/parcela/save':
+        ParcelaController::save();
+        break;
+    case '/parcela/get-by-id':
+        ParcelaController::getById();
+        break;
+    case '/parcela/delete':
+        ParcelaController::delete();
+        break;
+
+    /* Pagamento*/
 
     case '/pagamento':
         PagamentoController::index();
-        break;
-
-    case '/categoria_produto':
-        CategoriaProdutoController::index();
-        break;
-
-    case '/venda':
-        VendaController::index();
-        break;
-
-    case '/produto_venda':
-        ProdutoVendaController::index();
-        break;
-
-    /* SAVE */
-
-    case '/produto/save':
-        ProdutoController::save();
-        break;
-
-    case '/extrato/save':
-        ExtratoController::save();
-        break;
-
-    case '/parcela/save':
-        ParcelaController::save();
         break;
 
     case '/pagamento/save':
         PagamentoController::save();
         break;
 
-    case '/categoria_produto/save':
-        CategoriaProdutoController::save();
-        break;
-
-    case '/venda/save':
-        VendaController::save();
-        break;
-
-    case '/produto_venda/save':
-        ProdutoVendaController::save();
-        break;
-
-
-    /* GET-BY-ID */
-
-    case '/produto/get-by-id':
-        ProdutoController::getById();
-        break;
-
-    case '/extrato/get-by-id':
-        ExtratoController::getById();
-        break;
-
-    case '/parcela/get-by-id':
-        ParcelaController::getById();
-        break;
-
     case '/pagamento/get-by-id':
         PagamentoController::getById();
         break;
-
-    case '/categoria_produto/get-by-id':
-        CategoriaProdutoController::getById();
-        break;
-
-    case '/venda/get-by-id':
-        VendaController::getById();
-        break;
-
-    case '/categoria_produto/get-by-id':
-        CategoriaProdutoController::getById();
-        break;    
-
-    /* DELETE */
-
-    case '/produto/delete':
-        ProdutoController::delete();
-        break;
-
-    case '/extrato/delete':
-        ExtratoController::delete();
-        break;
-
-    case '/parcela/delete':
-        ParcelaController::delete();
-        break;
-
     case '/pagamento/delete':
         PagamentoController::delete();
+        break;
+
+    /* Categoria do Produto */
+    case '/categoria_produto':
+        CategoriaProdutoController::index();
+        break;
+    case '/categoria_produto/save':
+        CategoriaProdutoController::save();
+        break;
+    case '/categoria_produto/get-by-id':
+        CategoriaProdutoController::getById();
         break;
 
     case '/categoria_produto/delete':
         CategoriaProdutoController::delete();
         break;
 
+    /* Venda*/
+    case '/venda':
+        VendaController::index();
+        break;
+    case '/venda/save':
+        VendaController::save();
+        break;
+    case '/venda/get-by-id':
+        VendaController::getById();
+        break;
     case '/venda/delete':
         VendaController::delete();
         break;
 
+    /* Produto_Venda*/
+    case '/produto_venda':
+        ProdutoVendaController::index();
+        break;
+    case '/produto_venda/save':
+        ProdutoVendaController::save();
+        break;
     case '/produto_venda/delete':
         ProdutoVendaController::delete();
         break;
 
-
-        /* OUTROS */
-    
-    case '/login/auth':
-        LoginController::auth();
-        break;   
+    /* Outros */
     case '/home':
         CategoriaProdutoController::index();
         break;
 
     default:
         header('Location: /login');
-        break;    
+        break;
 }
