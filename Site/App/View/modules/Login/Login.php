8646ad62830738c68ebe3381f329d130bce40758
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-09-04 15:23:26 -0300

    feat: adicionando responsividade na tela de login

--- Site/App/View/modules/Login/Login.php
+++ Site/App/View/modules/Login/Login.php
@@ -11,39 +11,44 @@
 </head>
 
 <body>
-    <main class="container">
-        <section class="image-container">
-            <img src="View/assets/logo.png" id="imgLogo" alt="logo-Jahuepi">
-        </section>
-        <section class="input-container">
-            <form class="form" action="/login/auth" method="POST">
-                <label for="email">Endereço de E-mail:</label><br>
-                <input type="email" class="form-control" name="email" id="email"><br>
 
-                <label for="senha">Senha de Acesso:</label>
-                <div class="input-password">                    
-                    <input type="password" class="form-control" name="senha" id="senha"><br>
-                           
-                </div>
+    <div class="container-login">
+        <main class="container-white">
+            <section class="image-container">
+                <img src="View/assets/logo.png" id="imgLogo" alt="logo-Jahuepi">
+            </section>
+            <section class="input-container">
+                <form class="form" action="/login/auth" method="POST">
+                    <label for="email">Endereço de E-mail:</label><br>
+                    <input type="email" class="form-control" name="email" id="email"><br>
 
-                <div class="action-pass">
-                    <a href="">Esqueceu a senha?</a>   
-                    <box-icon name='show' id="verSenha"></box-icon>                 
-                </div>
+                    <label for="senha">Senha de Acesso:</label>
+                    <div class="input-password">
+                        <input type="password" class="form-control" name="senha" id="senha"><br>
 
-                <div>
-                    <?php if($loginFailed == true): ?>
-                        <h6 class="text-danger">Falha no login, tente novamente!</h6>
-                    <?php endif;?>
-                    
-                </div>
+                    </div>
 
-                <section class="btn-container">
-                    <button type="submit" class="btn btn-warning">Entrar</button>
-                </section>
-            </form>
-        </section>
-    </main>
+                    <div class="action-pass">
+                        <a href="">Esqueceu a senha?</a>
+                        <box-icon name='show' id="verSenha"></box-icon>
+                    </div>
+
+                    <div>
+                        <?php if ($loginFailed == true) : ?>
+                            <h6 class="text-danger">Falha no login, tente novamente!</h6>
+                        <?php endif; ?>
+
+                    </div>
+
+                    <section class="btn-container">
+                        <button type="submit" class="btn btn-warning">Entrar</button>
+                    </section>
+                </form>
+            </section>
+        </main>
+    </div>
+
+
 
     <?php include 'View/includes/js_config.php' ?>
     <script src="View/js/src/jquery.login.js"></script>
