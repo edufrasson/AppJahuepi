a4220b4cdaa91c784a775e0104a4736b0743b127
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-10-02 15:52:42 -0300

    fix: corrigindo detalhes

--- Site/App/View/modules/Venda/ModalProduto.php
+++ Site/App/View/modules/Venda/ModalProduto.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalProdutoTitle">Cadastrar Categoria</h5>
+                <h5 class="modal-title" id="modalProdutoTitle">Adicionar Produto</h5>
                 <!--<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>-->
                 </button>
