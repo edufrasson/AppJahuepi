0f038d463fefb10a669cfae634b6b13a614cc5f1
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-10-30 17:23:25 -0300

    feat: organização nos includes

--- Site/App/View/includes/js_config.php
+++ Site/App/View/includes/js_config.php
@@ -1,7 +1,10 @@
 <script src="View/js/jquery.config.min.js"></script>
 <script src="View/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!--<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>-->
+
+<!-- DataTable -->
 <script src="View/js/DataTable/datatables.min.js"></script>
+
 <script src="View/js/src/jquery.global.js"></script>
 <script src="View/js/plugin/sweetalert.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.min.js"></script>
