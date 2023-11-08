fd34ea6653e62a5f7734ad035ebd6b27d2b97347
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-11-08 15:04:21 -0300

    feat: finalizando resposividade da dashboard

--- Site/App/config.php
+++ Site/App/config.php
@@ -2,7 +2,7 @@
     define('BASEDIR', dirname(__FILE__, 2));
     define('VIEWS', BASEDIR . '/View/modules/');
 
-    $_ENV['db']['host'] = "localhost:3306";
+    $_ENV['db']['host'] = "localhost:3307";
     $_ENV['db']['user'] = "root";
-    $_ENV['db']['pass'] = "Eduardo@mysql";
+    $_ENV['db']['pass'] = "etecjau";
     $_ENV['db']['database'] = "db_jahuepi";
